52a65c1580ee22c17a7de627abedf374244a1a8e
terrific12 <91371551+terrific12@users.noreply.github.com>
2025-11-12 11:49:08 +0545

    adding student

---
+++ add_student.php
@@ -0,0 +1,22 @@
+
+<?php
+
+include 'config.php';
+
+header('Content-Type: application/json');
+//if there is any value show it else blank
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$age = $_POST['age'] ?? '';
+
+//if all the fields are filled
+if($name && $email && $age){
+    $stmt = $conn->prepare("INSERT INTO students (name, email, age) VALUES (?,?,?)");
+    $stmt ->execute([$name, $email, $age]);
+
+    //convert php array into JSON (sends and receives data between the server and client->browser/JS)
+    echo json_encode(['status' => 'success', 'message' => 'Student added successfully']);
+}else{
+    echo json_encode(['status' => 'error' , 'message' => 'All the fields are required']);
+}
+?>
